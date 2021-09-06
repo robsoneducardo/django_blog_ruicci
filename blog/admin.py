a7fd4d0b1cee762ef1c34f0b942b25abcecae5b8
robson <robsoneducardo@gmail.com>
2021-09-06 10:50:07 -0300

    Interface admin created

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+#admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "created", "updated")
+    prepopulated_fields = {"slug": ("title",)}
--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,10 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE())
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    #def __str__(self):
+        #return self.title
